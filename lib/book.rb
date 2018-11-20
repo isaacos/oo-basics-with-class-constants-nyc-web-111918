9f8a0a987f3eeb2f8a0e29320568f2ee588170a4
Isaac Oseas <oseasisaac91@gmail.com>
2018-11-20 21:10:23 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre
 
-  def initialize(title, genre)
+  def initialize(title)
     @title = title
     @genre = genre
   end
